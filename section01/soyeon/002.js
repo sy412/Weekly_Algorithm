fcbd407250d7526370946f02d2eda13fb8f0256a
soyeon.kim <84113107+sy412@users.noreply.github.com>
2023-04-06 22:37:27 +0900

    Update 002.js

--- section01/soyeon/002.js
+++ section01/soyeon/002.js
@@ -1,13 +1,21 @@
 // 삼각형 판별하기
 
 function solution(A, B, C) {
-  let answer = "Yes";
+  let answer = "YES";
   const sum = A + B + C;
   let max = A;
-  if (max < B) max = B;
-  else max = A;
-  if (max < C) max = C;
-  sum - max > max ? (answer = "YES") : (answer = "NO");
+  if (max < B) {
+    max = B;
+  } else {
+    max = A;
+  }
+  if (max < C) {
+    max = C;
+  }
+  if (sum - max <= max) {
+    answer = "NO";
+  }
+
   return answer;
 }
 console.log(solution(13, 33, 17));
